d704e87726e1166725470701b3208ce4c6c3f463
unknown <ufreezybg@gmail.com>
2015-04-23 22:55:16 +0300

    Re-adding the project.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//YOUR LOGIC HERE
+$path = './';
+$stylesPath = 'assets/styles/main.css';
+//AFTER HEADER LOGIC INCLUDE HEADER
+//TODO...REGISTER FORM/LOGIN
+include 'templates/header.php';
+
+//AFTER ARTICLES LOGIC INCLUDE ARTICLE
+include 'templates/categories.php';
+//TODO...CREATE ALL CATEGORIES - SUB CATEGORIES/POSTS/REPLY
+
+//AFTER FOOTER LOGIC INCLUDE FOOTER
+include 'templates/footer.php';
+//TODO...NOTHING AT THIS TIME (SOMETHING IN A FUTURE)
