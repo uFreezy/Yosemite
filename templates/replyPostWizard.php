06816773bf5a3ce0b4c67f29577a09bf913ed33c
unknown <ufreezybg@gmail.com>
2015-04-27 22:26:31 +0300

    Adding the reply function

--- templates/replyPostWizard.php
+++ templates/replyPostWizard.php
@@ -16,6 +16,7 @@
                     '<label class="topic-label" for="topic-reply-email">Email:</label>'.
                     '<input type="email" id="topic-reply-email" class="topic-input" required="required" placeholder="example@example.com" />';
                 }
+
                 ?>
                 <!--REPLY-TOPIC CONTENT-->
                 <label class="topic-label" for="topic-content">Your message...</label>
@@ -26,3 +27,81 @@
         </section>
     </div>
 </div>
+<?php
+    if(isset($_POST['status']) && isset($_POST['topicReplyContent'])) {
+        $date = date("Y/m/d");
+
+        // Checks if user is logged  and if there is saves the username in variable.
+        if(isset($_SESSION['username'])) {
+            $username = $_SESSION['username'];
+        }
+        // If user is not logged we check for input from the Guest form and if there isn't any we stop the script.
+        // If there is any we save it in the variable.
+        else {
+            if(isset($_POST['topicReplyName'])) {
+                $username = $_POST['topicReplyName'];
+            }
+            else {
+                exit();
+            }
+        }
+
+        // Get the name of the file in which the script is executed.
+        $fileID = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+
+        // Open the file.
+        $handle = fopen($fileID, "r");
+        $linecount = 0;
+
+        // Count the lines in the file.
+        while(!feof($handle)){
+            $line = fgets($handle);
+            $linecount++;
+        }
+        fclose($handle);
+
+        // Comment template to inject.
+        $template = '<div class="topic-comments">
+               <div>
+                   <a href="#" title="user">
+                       <img class="user-reply-image" src="../../assets/UI/icons/default-user-profile.png" alt="profile-image" width="90" height="90"/>
+                   </a>
+               </div>
+                <div class="reply-content">
+                    <h3>
+                        <a href="#" title="user">'. $username .'</a>
+                    </h3>
+                    <time datetime="11.11.1111" class="post-reply-date">'. $date .'</time>
+                    <p>'. $_POST['topicReplyContent'].'</p>
+            </div>
+            </div>
+            </article>
+            </section>
+            <?php
+            include "../footer.php";
+            ?>';
+
+        // Get the content from the topic file in array.
+        $contents = file($fileID);
+
+        // Remove the last 6 lines from the file.
+        for($i = $linecount; $i > $linecount - 6; $i--) {
+            $contents[$i] = '';
+
+        }
+
+        // Delete everything from the file so we can inject the new content.
+        $myfile = fopen($fileID, "w") or die("Unable to open file!");
+        fwrite($myfile, '');
+        fclose($myfile);
+
+        // Inject the new content.
+        for($i = 0; $i < sizeof($contents); $i++) {
+            file_put_contents($fileID, $contents[$i], FILE_APPEND);
+        }
+
+        // Inject the new comment.
+        file_put_contents($fileID, $template, FILE_APPEND);
+
+    }
+?>
